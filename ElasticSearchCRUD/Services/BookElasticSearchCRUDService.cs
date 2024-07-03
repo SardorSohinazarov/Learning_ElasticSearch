1f73c3c78a93b3c73cf500eb2983e34dff59fb4a
Sardor <studentx201@tuit.uz>
2024-07-03 14:24:37 +0500

    Add project files.

---
+++ ElasticSearchCRUD/Services/BookElasticSearchCRUDService.cs
@@ -0,0 +1,79 @@
+﻿using Elasticsearch.Net;
+using ElasticSearchCRUD.Models;
+using Nest;
+
+namespace ElasticSearchCRUD.Services
+{
+    public class BookElasticSearchCRUDService : IBookElasticSearchCRUDService
+    {
+        private readonly ElasticClient client;
+
+        public BookElasticSearchCRUDService()
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("books_index");
+
+            client = new ElasticClient(settings);
+        }
+
+        public async Task<bool> CreateBookAsync(Book book)
+        {
+            var indexResponse = await client.IndexDocumentAsync(book);
+            return indexResponse.IsValid;
+        }
+
+        public async Task<List<Book>> GetAllBooks()
+        {
+            var esResponse = (await client.SearchAsync<Book>()).Documents;
+
+            return esResponse.ToList();
+        }
+
+        public async Task<Book> GetBookAsync(int id)
+        {
+            var getResponse = await client.GetAsync<Book>(id);
+            if (getResponse.IsValid)
+                return getResponse.Source;
+
+            return null;
+        }
+
+        public async Task<bool> UpdateBookAsync(Book book)
+        {
+            var updateResponse = await client.UpdateAsync<Book>(book.Id, u => u
+                .Doc(book)
+                .Refresh(Refresh.True)
+            );
+
+            return updateResponse.IsValid;
+        }
+
+        public async Task<bool> DeleteBookAsync(int id)
+        {
+            var deleteResponse = await client.DeleteAsync<Book>(id);
+            return deleteResponse.IsValid;
+        }
+
+        public async Task<List<Book>> FuzzySearchBooksAsync(string query)
+        {
+            var searchResponse = await client.SearchAsync<Book>(s => s
+                .Query(q => q
+                    .MultiMatch(m => m
+                    .Fields(f => f
+                            .Field(p => p.Title)
+                            .Field(p => p.Description)
+                            .Field(p => p.Author)
+                        )
+                        .Query(query)
+                        .Fuzziness(Fuzziness.Auto)
+                    ))
+            );
+
+            if (searchResponse.IsValid)
+                return searchResponse.Documents.ToList();
+
+            return new List<Book>();
+        }
+    }
+
+}
---
+++ ElasticSearchCRUD/Services/IBookElasticSearchCRUDService.cs
@@ -0,0 +1,16 @@
+﻿using ElasticSearchCRUD.Models;
+
+namespace ElasticSearchCRUD.Services
+{
+    public interface IBookElasticSearchCRUDService
+    {
+        Task<bool> CreateBookAsync(Book book);
+        Task<List<Book>> GetAllBooks();
+        Task<Book> GetBookAsync(int id);
+        Task<bool> UpdateBookAsync(Book book);
+        Task<bool> DeleteBookAsync(int id);
+
+        Task<List<Book>> FuzzySearchBooksAsync(string query);
+    }
+
+}
